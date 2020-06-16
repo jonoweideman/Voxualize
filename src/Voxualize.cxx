64736d280462a7ff67012bcfd209141acd850307
Jonathan Weideman <jonoweideman1@gmail.com>
2020-06-16 14:15:49 +0200

    Add .gitignore and make some changes to correct data extent

--- src/Voxualize.cxx
+++ src/Voxualize.cxx
@@ -46,31 +46,26 @@ int main(int argc, char *argv[])
   else
   {
     // Read raw binary file data into float array
-    float* array = new float[540*450*201];
     struct DataCube dataCube;
     dataCube.fileName = argv[1];
-    dataCube.dimx = 540; dataCube.dimy = 450; dataCube.dimz = 201;
+    dataCube.dimx = 256; dataCube.dimy = 256; dataCube.dimz = 159;
     dataCube.num_pixels = dataCube.dimx * dataCube.dimy * dataCube.dimz;
-    dataCube.array = array;
-    readFromFile(&dataCube, array);
+    dataCube.array = new float[dataCube.num_pixels];
+    readFromFile(&dataCube, dataCube.array);
 
     // Construct vtkFloatArray from this float array. No copying is done here
     vtkSmartPointer<vtkFloatArray> floatArray = vtkSmartPointer<vtkFloatArray>::New();
     floatArray->SetName("Float Array");
-    floatArray->SetArray(array, dataCube.num_pixels, 1);
+    floatArray->SetArray(dataCube.array, dataCube.num_pixels, 1);
 
     // Create vtkImageImport object in order to use an array as input data to the volume mapper.
     vtkSmartPointer<vtkImageImport> imageImport = vtkSmartPointer<vtkImageImport>::New();
     imageImport -> SetDataScalarTypeToFloat();
-    imageImport-> SetWholeExtent(0,256,0,256,0,159);
+    imageImport-> SetWholeExtent(0,dataCube.dimx - 1,0,dataCube.dimy - 1,0,dataCube.dimz - 1);
     imageImport ->SetDataExtentToWholeExtent();
-
-    imageImport->SetImportVoidPointer(floatArray->GetVoidPointer(0)); // Not sure what valueIdx to use.
-                                                    // This is probably the issue. This function requires a void *.
-                                                    // Other options are SetInputConnection(vtkAlgorithmOutput*) and SetInputData(vtkDataObject*)
-                                                    // NOTE: vtkFloatArray is not a vtkDataObject apparently.
+    imageImport->SetImportVoidPointer(floatArray->GetVoidPointer(0));
     imageImport->SetNumberOfScalarComponents(1);
-    imageImport -> SetDataSpacing(1,1,1); // Default values
+    imageImport -> SetDataSpacing(1.0,double(dataCube.dimx) / dataCube.dimy, (double)dataCube.dimx / dataCube.dimz);
     imageImport -> Update();
 
     // The mapper / ray cast function know how to render the data
@@ -99,13 +94,15 @@ int main(int argc, char *argv[])
     // Data values for ds9.arr 540x450x201 are in range [-0.139794;0.153026]
     vtkSmartPointer<vtkPiecewiseFunction> opacityTransferFunction =
       vtkSmartPointer<vtkPiecewiseFunction>::New();
-    opacityTransferFunction->AddPoint(-0.14, 0.0);
+    //opacityTransferFunction->AddPoint(-0.14, 0.0);
+    opacityTransferFunction->AddPoint(-0.0, 0.0);
     opacityTransferFunction->AddPoint(0.16, 1.0);
 
     // Create transfer mapping scalar value to color
     vtkSmartPointer<vtkColorTransferFunction> colorTransferFunction =
       vtkSmartPointer<vtkColorTransferFunction>::New();
-    colorTransferFunction->AddRGBPoint(-0.14, 0.0, 0.0, 0.0);
+    //colorTransferFunction->AddRGBPoint(-0.14, 0.0, 0.0, 0.0);
+    colorTransferFunction->AddRGBPoint(-0.0, 0.0, 0.0, 0.0);
     colorTransferFunction->AddRGBPoint(0.16, 1.0, 1.0, 1.0);
     //colorTransferFunction->AddRGBPoint(0.16, 1.0, 0.0, 0.7);
 
@@ -114,8 +111,8 @@ int main(int argc, char *argv[])
       vtkSmartPointer<vtkVolumeProperty>::New();
     volumeProperty->SetColor(colorTransferFunction);
     volumeProperty->SetScalarOpacity(opacityTransferFunction);
-    volumeProperty->ShadeOn();
-    volumeProperty->SetInterpolationTypeToLinear();
+    //volumeProperty->ShadeOn();
+    volumeProperty->SetInterpolationTypeToNearest();
 
     // The volume holds the mapper and the property and
     // can be used to position/orient the volume
@@ -173,9 +170,9 @@ int main(int argc, char *argv[])
 }
 
 void readFromFile(DataCube* dataCube, float* array){
-  size_t num_pixels = (*dataCube).dimx*(*dataCube).dimy*(*dataCube).dimz;
+  size_t num_pixels = dataCube->dimx*dataCube->dimy*dataCube->dimz;
   std::cout<<num_pixels<<std::endl;
-  std::ifstream input_file((*dataCube).fileName, ios::binary);
-  std::cout<<(*dataCube).fileName<<std::endl;
+  std::ifstream input_file(dataCube->fileName, ios::binary);
+  std::cout<<dataCube->fileName<<std::endl;
   input_file.read((char*) array, num_pixels * sizeof(float));
 }
