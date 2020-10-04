88088415fc27fcca39a85c4e6bb04d6fb2ebc33f
Jonathan Weideman <jonoweideman1@gmail.com>
2020-10-04 15:19:10 +0200

    Cleanup code, and add m81.arr file

--- src/voxualize_server.cxx
+++ src/voxualize_server.cxx
@@ -177,28 +177,13 @@ class GreeterServiceImpl final : public Greeter::Service {
     
     mtx.lock();
     cout << "GetHQRender rpc" << endl;
-    // if (!is_egl_started){
-    //   createEGLRenderOnServer();
-    //   is_egl_started = true;
-    // }
+
     // Get pointer to data.
     pixelData = updateCameraAndGetData(request); //and save screenshot....for now.
 
-    float * pixelDataFloats = reinterpret_cast<float *>(pixelData);
-    // for ( int i=180000; i<180100; i++){
-    //   cout << *(pixelDataFloats+1) << ' ';
-    // }
-
-    // Encode with NVENC / FFMPEG ? Again return a pointer to encoded data
+    // Encode with FFMPEG.
     AVPacket * pkt = encodePixelData(pixelData, request);
-    //reply->set_size_in_bytes(pkt->size);
-    // reply->window_width(1836);
-    // reply->window_height(500);
-    // for ( int i=180000; i<180100; i++){
-    //   cout << *(pixelDataFloats+1) << ' ';
-    // }
-    //number_of_bytes = imageData->GetNumberOfPoints()*4;
-    //reply->set_size_in_bytes(imageData->GetNumberOfPoints()*4);
+
     streamHQRender(writer);
     mtx.unlock();
     return Status::OK;
@@ -206,7 +191,9 @@ class GreeterServiceImpl final : public Greeter::Service {
 
   Status GetNewROILOD(ServerContext *context, const CameraInfo *request,
                             ServerWriter<ROILOD> *writer) override {
+    // Given the cropping planes info, compute new LOD model.
     mtx.lock();
+    cout << "GetNewROILOD rpc" << endl;
     switch (request->s_method()) {
       case CameraInfo::Max :
         dataCube.s_method = "Max";
@@ -218,34 +205,30 @@ class GreeterServiceImpl final : public Greeter::Service {
         dataCube.s_method = "Max";
         break;
     }
-    // Given the cropping planes info, compute new LOD model.
-    cout << "GetNewROILOD rpc" << endl;
+    
     const google::protobuf::RepeatedField<float> cplanes = request->cropping_planes();
     float cropping_dims[6];
     cropping_dims[0] = cplanes.Get(0); cropping_dims[1] = cplanes.Get(1); cropping_dims[2] = cplanes.Get(2);
     cropping_dims[3] = cplanes.Get(3); cropping_dims[4] = cplanes.Get(4); cropping_dims[5] = cplanes.Get(5);
 
-    cout << "Request for new ROI, memsize or both detected." << endl;
     dataCube.generateLODModelNew(request->target_size_lod_bytes(), &cropping_dims[0]);
 
-    // reply->set_true_size_lod_bytes(dataCube.LOD_num_bytes);
-    // reply->add_dimensions_lod(dataCube.new_dim_x);
-    // reply->add_dimensions_lod(dataCube.new_dim_y);
-    // reply->add_dimensions_lod(dataCube.new_dim_z);
     streamLODModel(writer);
     cout << "Finished GetNewROILOD rpc" << endl;
     mtx.unlock();
     return Status::OK;
   }
 
+  // Reset Cropping Planes to Full.
   Status Reset(ServerContext *context, const CameraInfo *request,
                             Empty *reply) override {
     dataCube.generateLODModel(request->target_size_lod_bytes());
     return Status::OK;
   }
 
+  // Setup VTK EGL render on server
   void createEGLRenderOnServer(){
-    cout << "Creating EGL render on server." << endl;
+    cout << "Setting up VTK objects." << endl;
     floatArray = vtkSmartPointer<vtkFloatArray>::New();
     floatArray->SetName("Float Array");
     floatArray->SetArray(dataCube.floatArray, dataCube.num_pixels, 1);
@@ -257,7 +240,6 @@ class GreeterServiceImpl final : public Greeter::Service {
     imageImport->SetDataExtentToWholeExtent();
     imageImport->SetImportVoidPointer(floatArray->GetVoidPointer(0));
     imageImport->SetNumberOfScalarComponents(1);
-    //imageImport->SetDataSpacing(1.0, (double)(dataCube.dimx) / dataCube.dimy, (double)(dataCube.dimx) / dataCube.dimz);
     imageImport->SetDataSpacing(1.0, 1.0, 1.0);
     imageImport->Update();
 
@@ -306,17 +288,7 @@ class GreeterServiceImpl final : public Greeter::Service {
     ren1->GetActiveCamera()->Elevation(30);
     ren1->ResetCameraClippingRange();
     ren1->ResetCamera();
-    //renWin->SwapBuffersOff();
-    cout << "Is current? : " << renWin->IsCurrent() << endl;
-    renWin->SetSize(1836, 500);
 
-    //renWin->Render();
-    //renWin->Frame();
-    //renWin->WaitForCompletion();
-    //captureScreenShotOfCurrentEGLRender();
-
-    double * position = ren1->GetActiveCamera()->GetFocalPoint();
-    cout << position[0] << ' ' << position[1] << ' ' << position[2] << endl;
     cout << "Finished setting up EGL render on server." << endl;
     return;
   }
@@ -339,49 +311,27 @@ class GreeterServiceImpl final : public Greeter::Service {
     renWin -> Finalize();
     renWin-> Initialize();
     renWin->SetSize(window_width, window_height);
-    // Set variables.
-    //ren1->ResetCamera();
+
     ren1->GetActiveCamera()->SetPosition(position.Get(0)*dataCube.x_scale_factor,position.Get(1)*dataCube.y_scale_factor,position.Get(2)*dataCube.z_scale_factor);
     ren1->GetActiveCamera()->SetViewUp(view_up.Get(0),view_up.Get(1),view_up.Get(2));
     ren1->GetActiveCamera()->SetFocalPoint(focal_point.Get(0)*dataCube.x_scale_factor,focal_point.Get(1)*dataCube.y_scale_factor,focal_point.Get(2)*dataCube.z_scale_factor);
-    //ren1->GetActiveCamera()->SetDistance(distance);
     colorTransferFunction->RemoveAllPoints();
     colorTransferFunction->AddRGBPoint(0.0, 0.0, 0.0, 0.0);
     colorTransferFunction->AddRGBPoint(0.0, rgb.Get(0)/255, rgb.Get(1)/255, rgb.Get(2)/255);
-    //colorTransferFunction->AddRGBPoint(round(dataCube.min_pixel_val*10)/10, rgb.Get(0)/255, rgb.Get(1)/255, rgb.Get(2)/255);
     colorTransferFunction->AddRGBPoint(round(dataCube.max_pixel_val*10)/10, 1.0, 1.0, 1.0);
 
-
-    cout << "Trying to set cropping planes" << endl;
-    cout << "Requested cropping planes: " << cplanes.Get(0) << ' ' << cplanes.Get(1) << ' ' << cplanes.Get(2) << ' ' << 
-                                            cplanes.Get(3) << ' ' << cplanes.Get(4) << ' ' << cplanes.Get(5)<< endl;
-    cout << "Cropping planes set on backend: " << cplanes.Get(0)*dataCube.x_scale_factor << ' ' << cplanes.Get(1)*dataCube.x_scale_factor 
-                                          << ' ' << cplanes.Get(2)*dataCube.y_scale_factor << ' ' << cplanes.Get(3)*dataCube.y_scale_factor 
-                                          << ' ' << cplanes.Get(4)*dataCube.z_scale_factor << ' ' << cplanes.Get(5)*dataCube.z_scale_factor << endl;
-
-   volumeMapper->SetCroppingRegionPlanes(cplanes.Get(0)*dataCube.x_scale_factor, cplanes.Get(1)*dataCube.x_scale_factor, 
+    volumeMapper->SetCroppingRegionPlanes(cplanes.Get(0)*dataCube.x_scale_factor, cplanes.Get(1)*dataCube.x_scale_factor, 
                                         cplanes.Get(2)*dataCube.y_scale_factor, cplanes.Get(3)*dataCube.y_scale_factor, 
                                         cplanes.Get(4)*dataCube.z_scale_factor, cplanes.Get(5)*dataCube.z_scale_factor);
-    //cout << "Sleep 1..." << endl;
-    //sleep(5);
-    //renWin->SwapBuffersOff();
+
     renWin->AddRenderer(ren1);
-    cout << "Is current? : " << renWin->IsCurrent() << endl;
     renWin->Render();
-    //sleep(5);
     renWin->Frame();
-    //cout << "Sleep 3..." << endl;
-    //sleep(5);
+
     renWin->WaitForCompletion();
-    //renWin->CopyResultFrame();
-    //cout << "Done!" << endl;
-    cout << "Is current? : " << renWin->IsCurrent() << endl;
 
     oldSB = renWin->GetSwapBuffers();
     renWin->SwapBuffersOff();
-
-    // //restore swapping state
-    // renWin->SetSwapBuffers(oldSB);
 
     captureScreenShotOfCurrentEGLRender();
 
@@ -394,27 +344,16 @@ class GreeterServiceImpl final : public Greeter::Service {
 
     imageData = windowToImageFilter->GetOutput();
 
-    //cout << imageData->GetNumberOfCells() << endl;
-    //cout << imageData->GetNumberOfPoints() << endl;
-
     int* dims = imageData->GetDimensions();
-    std::cout << "Dims: " << " x: " << dims[0] << " y: " << dims[1] << " z: " << dims[2] << std::endl;
-    //imageData -> PrintSelf(cout, vtkIndent(2)) ;
     pointer = static_cast<unsigned char *>(imageData->GetScalarPointer(0,0,0));
-    //pointer = reinterpret_cast<unsigned char *>(renWin->GetPixelData(0,599,0, 599, 0, 0));
-    // restore swapping state
-    for ( int i=0; i<100; i++){
-      cout << +*(pointer+i) << ' ';
-    }
+
     renWin->SetSwapBuffers(oldSB);
-    //renWin->Initialize();
-    //renWin->AddRenderer(ren1);
+
     return pointer;
   }
   
-  // Encode the data. For now using ffmpeg. Hopefully in future done using GPU acceleration.
+  // Encode the data using ffmpeg.
   AVPacket * encodePixelData(unsigned char *pixelData, const CameraInfo *request){
-    cout << "Attempting to encode" << endl;
     const AVCodec *codec;
     AVCodecContext *c= NULL;
     int i, ret, x, y, got_output;
@@ -442,7 +381,6 @@ class GreeterServiceImpl final : public Greeter::Service {
     c->height = window_height;
     /* frames per second */
     c->time_base = (AVRational){1, 1};
-    //c->framerate = (AVRational){1, 1};
 
     c->gop_size = 0; //Intra only - meaning the pictures should be constructed only from information
                       // within that picture, and not any other pictures.
@@ -485,8 +423,6 @@ class GreeterServiceImpl final : public Greeter::Service {
                                       AV_PIX_FMT_RGB24, c->width, c->height,
                                       AV_PIX_FMT_YUVJ420P, 0, 0, 0, 0);
 
-    //ctx->color_range = AVCOL_RANGE_JPEG;
-
     av_init_packet(pkt);
     pkt->data = NULL;    // packet data will be allocated by the encoder
     pkt->size = 0;
@@ -508,41 +444,15 @@ class GreeterServiceImpl final : public Greeter::Service {
         exit(7);
     }
 
-    FILE *f;
-    const char *filename = "outfile.mpg";
-    uint8_t endcode[] = { 0, 0, 1, 0xb7 };
-    f = fopen(filename, "wb");
     i = 0;
-    if (!f) {
-      fprintf(stderr, "Could not open %s\n", filename);
-      exit(4);
-    }
-    if (got_output) {
-      printf("Write frame %3d (size=%5d)\n", i, pkt->size);
-      fwrite(pkt->data, 1, pkt->size, f);
-      //av_free_packet(pkt);
-    }
 
     for (got_output = 1; got_output; i++) {
       fflush(stdout);
-      
       ret = avcodec_encode_video2(c, pkt, NULL, &got_output);
-      if (ret < 0) {
-        fprintf(stderr, "Error encoding frame\n");
-        exit(8);
-      }
-      
       if (got_output) {
-        printf("Write frame %3d (size=%5d)\n", i, pkt->size);
-        
-        fwrite(pkt->data, 1, pkt->size, f);
         return pkt;
-        //av_free_packet(pkt);
       }
     }
-    /* add sequence end code to have a real mpeg file */
-    fwrite(endcode, 1, sizeof(endcode), f);
-    fclose(f);
 
     avcodec_close(c);
     av_free(c);
@@ -585,30 +495,6 @@ class GreeterServiceImpl final : public Greeter::Service {
   }
 
   void streamLODModel(ServerWriter<ROILOD> *writer){
-    //char * bytes = dataCube.getBytePointerLODModel();
-    //Do zfp compression.
-    // cout << "Zfp compressing LOD model..." << endl;
-    // int status = Compress(dataCube.LODFloatArray,  &compression_buffer, &compression_size, 
-    //                       dataCube.new_dim_x, dataCube.new_dim_y, dataCube.new_dim_z, 11);
-    // cout << "Finished compressing." << endl;
-    // char * bytes = compression_buffer.data();
-    // ROILOD d;
-    // d.set_total_lod_bytes(dataCube.LOD_num_bytes);
-    // d.add_dimensions_lod(dataCube.new_dim_x);
-    // d.add_dimensions_lod(dataCube.new_dim_y);
-    // d.add_dimensions_lod(dataCube.new_dim_z);
-    // for (int i = 0; i < compression_size; i += bytes_per_write){
-    //   if ( compression_size - i < bytes_per_write){
-    //     d.set_bytes(bytes, compression_size - i);
-    //     d.set_num_bytes(compression_size - i);
-    //   } else {
-    //     d.set_bytes(bytes, bytes_per_write);
-    //     d.set_num_bytes(bytes_per_write);
-    //   }
-    //   writer->Write(d);
-    //   bytes += bytes_per_write; // Update the pointer.
-    // }
-
     char * bytes = dataCube.getBytePointerLODModel();
     ROILOD d;
     d.set_total_lod_bytes(dataCube.LOD_num_bytes);
@@ -628,46 +514,59 @@ class GreeterServiceImpl final : public Greeter::Service {
         bytes += bytes_per_write; // Update the pointer.
       }
       writer->Write(d);
-      //bytes += bytes_per_write; // Update the pointer.
+    }
+  }
+
+  void streamZfpCompressedLODModel(ServerWriter<ROILOD> *writer){
+    //Do zfp compression.
+    int status = Compress(dataCube.LODFloatArray,  &compression_buffer, &compression_size, 
+                          dataCube.new_dim_x, dataCube.new_dim_y, dataCube.new_dim_z, 11);
+
+    char * bytes = compression_buffer.data();
+    ROILOD d;
+    d.set_total_lod_bytes(dataCube.LOD_num_bytes);
+    d.add_dimensions_lod(dataCube.new_dim_x);
+    d.add_dimensions_lod(dataCube.new_dim_y);
+    d.add_dimensions_lod(dataCube.new_dim_z);
+    for (int i = 0; i < compression_size; i += bytes_per_write){
+      if ( compression_size - i < bytes_per_write){
+        d.set_bytes(bytes, compression_size - i);
+        d.set_num_bytes(compression_size - i);
+      } else {
+        d.set_bytes(bytes, bytes_per_write);
+        d.set_num_bytes(bytes_per_write);
+      }
+      writer->Write(d);
+      bytes += bytes_per_write; // Update the pointer.
     }
   }
 
   void streamHQRender(ServerWriter<HQRender> *writer){
     cout << "Streaming HQ Render" << endl;
     char * encodedData = reinterpret_cast<char *>(encodedFramePkt.data);
-    //char * encodedData = reinterpret_cast<char *>(pixelData);
-    // for ( int i=100000; i<100100; i++){
-    //   cout << +*(encodedData+i) << ' ';
-    // }
     int num_bytes_tmp  = encodedFramePkt.size;
-    //int num_bytes_tmp = number_of_bytes;
 
-    cout << "Starting to write encoded frame to stream: " << bytes_per_write << ' ' << num_bytes_tmp << endl;
     HQRender d;
     d.set_size_in_bytes(num_bytes_tmp);
     d.set_width(window_width);
     d.set_height(window_height);
 
     for (int i = 0; i < num_bytes_tmp; i += bytes_per_write){
-      //cout << "Hello" << endl;
       if ( num_bytes_tmp - i < bytes_per_write){
         cout << num_bytes_tmp - i << endl;
         d.set_bytes(encodedData, num_bytes_tmp - i);
         d.set_num_bytes(num_bytes_tmp - i);
-        //encodedData += bytes_per_write; // Update the pointer.
       } else {
         cout << bytes_per_write << endl;
         d.set_bytes(encodedData, bytes_per_write);
         d.set_num_bytes(bytes_per_write);
         encodedData += bytes_per_write; // Update the pointer.
       }
-      //encodedData += bytes_per_write; // Update the pointer.
       writer->Write(d);
-      // encodedData += bytes_per_write; // Update the pointer.
-      // cout << bytes_per_write;
     }
   }
 
+  // ZFP compression.
   int Compress(float* array, vector<char>* compression_buffer, size_t* compressed_size, 
                uint32_t nx, uint32_t ny, uint32 nz, uint32_t precision) {
     int status = 0;     /* return value: 0 = success */
@@ -706,8 +605,7 @@ class GreeterServiceImpl final : public Greeter::Service {
     stream_close(stream);
 
     return status;
-}
-
+  }
 };
 
 void RunServer()
@@ -734,9 +632,6 @@ void RunServer()
 
 int main(int argc, char *argv[])
 {
-  // DataCube dataCube;
-  // dataCube.createCube("ds9.arr");
-  // renderLODModelOnServer(&dataCube);
   RunServer();
   return 0;
 }
